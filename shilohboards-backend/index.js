103d990b309cdd922827d24dd78a82336ebfdba2
thesomeoldguy <102364953+thesomeoldguy@users.noreply.github.com>
2025-03-02 12:38:49 -0500

    Refactor variables

--- shilohboards-backend/index.js
+++ shilohboards-backend/index.js
@@ -50,6 +50,30 @@ const alphabetData = {
     Z: { object: "Zipper", sound: "z.mp3", image: "Zipper.png"},
 };
 
+const numbersData = {
+    1: { object: "Car", sound: "one.mp3", image: "one-car.png" },
+    2: { object: "Shoes", sound: "two.mp3", image: "two-shoes.png" },
+    3: { object: "Guitars", sound: "three.mp3", image: "three-guitars.png" },
+    4: { object: "Icecreams", sound: "four.mp3", image: "four-icecreams.png" },
+    5: { object: "Stars", sound: "five.mp3", image: "five-stars.png" },
+    6: { object: "Eggs", sound: "six.mp3", image: "six-eggs.png" },
+    7: { object: "Banana", sound: "seven.mp3", image: "seven-bananas.png" },
+    8: { object: "Crayons", sound: "eight.mp3", image: "eight-crayons.png" },
+    9: { object: "Spoons", sound: "nine.mp3", image: "nine-spoons.png" },
+    10: { object: "Apples", sound: "ten.mp3", image: "ten-apples.png" },
+    11: { object: "Jellyfish", sound: "eleven.mp3", image: "eleven-jellyfish.png" },
+    12: { object: "Hats", sound: "twelve.mp3", image: "twelve-hats.png" },
+    13: { object: "Balloons", sound: "thirteen.mp3", image: "thirteen-balloons.png" },
+    14: { object: "Socks", sound: "fourteen.mp3", image: "fourteen-socks.png" },
+    15: { object: "Trees", sound: "fifteen.mp3", image: "fifteen-trees.png" },
+    16: { object: "Penguins", sound: "sixteen.mp3", image: "sixteen-penguins.png" },
+    17: { object: "Shells", sound: "seventeen.mp3", image: "seventeen-shells.png" },
+    18: { object: "Candy", sound: "eighteen.mp3", image: "eighteen-candies.png" },
+    19: { object: "Books", sound: "nineteen.mp3", image: "nineteen-books.png" },
+    20: { object: "Cupcakes", sound: "twenty.mp3", image: "twenty-cupcakes.png" },
+};
+
+
 app.post('/create-parent', async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -261,29 +285,6 @@ app.get("/alphabet/score/:childId", async (req, res) => {
     }
 });
 
-
-const numbersData = {
-    1: { object: "Car", sound: "one.mp3", image: "one-car.png" },
-    2: { object: "Shoes", sound: "two.mp3", image: "two-shoes.png" },
-    3: { object: "Guitars", sound: "three.mp3", image: "three-guitars.png" },
-    4: { object: "Icecreams", sound: "four.mp3", image: "four-icecreams.png" },
-    5: { object: "Stars", sound: "five.mp3", image: "five-stars.png" },
-    6: { object: "Eggs", sound: "six.mp3", image: "six-eggs.png" },
-    7: { object: "Banana", sound: "seven.mp3", image: "seven-bananas.png" },
-    8: { object: "Crayons", sound: "eight.mp3", image: "eight-crayons.png" },
-    9: { object: "Spoons", sound: "nine.mp3", image: "nine-spoons.png" },
-    10: { object: "Apples", sound: "ten.mp3", image: "ten-apples.png" },
-    11: { object: "Jellyfish", sound: "eleven.mp3", image: "eleven-jellyfish.png" },
-    12: { object: "Hats", sound: "twelve.mp3", image: "twelve-hats.png" },
-    13: { object: "Balloons", sound: "thirteen.mp3", image: "thirteen-balloons.png" },
-    14: { object: "Socks", sound: "fourteen.mp3", image: "fourteen-socks.png" },
-    15: { object: "Trees", sound: "fifteen.mp3", image: "fifteen-trees.png" },
-    16: { object: "Penguins", sound: "sixteen.mp3", image: "sixteen-penguins.png" },
-    17: { object: "Shells", sound: "seventeen.mp3", image: "seventeen-shells.png" },
-    18: { object: "Candy", sound: "eighteen.mp3", image: "eighteen-candies.png" },
-    19: { object: "Books", sound: "nineteen.mp3", image: "nineteen-books.png" },
-    20: { object: "Cupcakes", sound: "twenty.mp3", image: "twenty-cupcakes.png" },
-};
 
 const allNumbers = Object.keys(numbersData).map(Number);
 
